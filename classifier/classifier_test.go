3b67a0d6e951f35b7695e1a666775393324c27ca
Tomoya Kose <tomoya@mitsuse.jp>
2015-07-28 21:38:46 +0900

    Write test cases for "classifier.New".

--- classifier/classifier_test.go
+++ classifier/classifier_test.go
@@ -1,1 +1,40 @@
 package classifier
+
+import (
+	"testing"
+)
+
+type constructionTest struct {
+	classSize  int
+	dimensions int
+}
+
+func TestNewSucceeds(t *testing.T) {
+	test := constructionTest{
+		classSize:  4,
+		dimensions: 8,
+	}
+
+	defer func() {
+		if p := recover(); p == nil {
+			return
+		}
+
+		t.Fatal("New should not panic for one or more classes and positive dimmensions.")
+	}()
+	New(test.classSize, test.dimensions)
+}
+
+func TestNewPanicsForNonPositiveClassSize(t *testing.T) {
+	test := constructionTest{
+		classSize:  0,
+		dimensions: 8,
+	}
+
+	defer func() {
+		if p := recover(); p == nil {
+			t.Fatal("The size of claasses should be one or more.")
+		}
+	}()
+	New(test.classSize, test.dimensions)
+}
