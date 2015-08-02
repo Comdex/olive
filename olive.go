59bb23431e2d3a8fbb8bef78c7b6d32e3ca0e172
Tomoya Kose <tomoya@mitsuse.jp>
2015-08-02 22:38:45 +0900

    Learning algorithms should satisfy "Learner" interface.

--- olive.go
+++ olive.go
@@ -2,3 +2,37 @@
 Package "olive" provides online algorithms for machine learning.
 */
 package olive
+
+import (
+	. "github.com/mitsuse/matrix-go"
+	"github.com/mitsuse/olive/classifier"
+	"github.com/mitsuse/olive/internal/validates"
+)
+
+type Learner interface {
+	Learn(c *classifier.Classifier, instances []*Instance) *classifier.Classifier
+}
+
+type Instance struct {
+	feature Matrix
+	label   int
+}
+
+func NewInstance(feature Matrix, label int) *Instance {
+	validates.ShouldBeFeature(feature)
+
+	i := &Instance{
+		feature: feature,
+		label:   label,
+	}
+
+	return i
+}
+
+func (i *Instance) Feature() Matrix {
+	return i.feature
+}
+
+func (i *Instance) Label() int {
+	return i.label
+}
--- olive_test.go
+++ olive_test.go
@@ -1,1 +1,52 @@
 package olive
+
+import (
+	"testing"
+
+	"github.com/mitsuse/matrix-go/dense"
+	"github.com/mitsuse/olive/classifier"
+)
+
+func TestInstanceFailsForNonFeatureMatirx(t *testing.T) {
+	m := dense.New(2, 3)(
+		0, 1, 2,
+		1, 0, -1,
+	)
+
+	label := 4
+
+	defer func() {
+		if p := recover(); p == classifier.NON_FEATURE_MATRIX_PANIC {
+			return
+		}
+
+		t.Fatal("NewInstance should use \"validate.ShouldBeFeature\".")
+	}()
+	NewInstance(m, label)
+}
+
+func TestInstanceFeature(t *testing.T) {
+	feature := dense.New(1, 6)(0, 1, 2, 1, 0, -1)
+	label := 4
+	instance := NewInstance(feature, label)
+
+	m := dense.New(1, 6)(0, 1, 2, 1, 0, -1)
+
+	if instance.Feature().Equal(m) {
+		return
+	}
+
+	t.Fatal("The feature vector should not be modified on creating instance.")
+}
+
+func TestInstanceLabel(t *testing.T) {
+	feature := dense.New(1, 6)(0, 1, 2, 1, 0, -1)
+	label := 4
+	instance := NewInstance(feature, label)
+
+	if instance.Label() == label {
+		return
+	}
+
+	t.Fatal("The label should not be modified on creating instance.")
+}
